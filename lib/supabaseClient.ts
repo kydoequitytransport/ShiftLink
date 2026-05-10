af7e7f089fc7469509a9b0151b4f83c24a48a9cb
kydoequitytransport <kydo@fitrewards.io>
2026-05-10 06:57:05 +0000

    Fix

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.STORAGE_SUPABASE_URL as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_STORAGE_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_STORAGE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
